2bfb0357554af3b0a2b0b8ff5e6818709fc63335
AllieXinZhao <xinallie@gmail.com>
2020-02-01 18:13:24 -0500

    Added bullet class

---
+++ Repair-Game/Assets/Script/DelayBut.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayBut : MonoBehaviour
+{
+    //Allie Zhao
+    public GameObject player;
+    private Vector3 playerP;//player's position OR gun's position
+    private Vector3 position;
+    private Vector3 vel;//same driction with player froward
+    public int speed;//max buullet seppd
+    public float butR;
+    public float targetR;
+    public Vector3 targetP;
+
+    private bool stay;
+    private float stayTime;
+    // Start is called before the first frame update
+    void Start()
+    {        //Allie Zhao
+        playerP = player.transform.position;
+        //vel = player.transform.forward;
+        vel = new Vector3(1, 0, 1).normalized;
+        position = transform.position;
+        speed = 5;
+        butR = 1.0f;
+        targetR = 1.0f;
+        targetP = new Vector3(10, 1, 10);
+
+        ///
+        stay = false;
+        stayTime = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(stay == false) Move();
+        CollDect(targetP, targetR);
+    }
+
+    //Author: Allie Zhao
+    // 3 stage attack
+    //The bullet will deatroy after attack 3 times
+    private void DelaBult()
+    {
+        stay = true;
+        position = targetP - vel * 1f;
+        transform.position = position;
+        stayTime -= Time.deltaTime;
+        if (stayTime <= 0.6f)
+        {
+            position = targetP - vel * 0.5f;
+            transform.position = position;
+        }
+        if (stayTime <= 0.3f)
+        {
+            position = targetP - vel * 0.1f;
+            transform.position = position;
+        }
+        if (stayTime <= 0)
+        {
+            DestroyBult();
+        }
+    }
+
+    private void Move()
+    {
+        position += vel * speed * Time.deltaTime;
+        transform.position = position;
+    }
+
+    private void CollDect(Vector3 targetPosition, float targectRaadius)
+    {
+        float dist = Vector3.Distance(targetPosition, position);
+        if (dist < butR + targectRaadius)
+        {//destory the bullet
+         //DestroyBult();
+            DestroyBult();
+        }
+    }
+    //Author: Allie Zhao
+    //destroy the bullet
+    private void DestroyBult()
+    {
+        gameObject.SetActive(false);
+
+    }
+}
---
+++ Repair-Game/Assets/Script/RotatedBut.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatedBut : MonoBehaviour
+{
+    //Allie Zhao
+    public GameObject player;
+    private Vector3 playerP;//player's position OR gun's position
+    private Vector3 position;
+    private Vector3 vel;//same driction with player froward
+    public int speed;//max buullet seppd
+    public float butR;
+    public float targetR;
+    public Vector3 targetP;
+
+    private float angle;
+    private float radius;
+    private float coldDownTime;
+
+
+
+    private float dist;
+    private float degree;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Allie Zhao
+        playerP = player.transform.position;
+        //vel = player.transform.forward;
+        vel = new Vector3(1, 0, 1).normalized;
+        position = transform.position;
+        speed = 5;
+        butR = 1.0f;
+        targetR = 5.0f;
+        targetP = new Vector3(10, 1, 10);
+        dist = Random.Range(8f, 12f);
+        degree = Random.Range(0f, 180f);
+        SetRotatedValue(degree, dist, 20f);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        GoAround();
+        CollDect(targetP, targetR);
+    }
+
+    public void SetRotatedValue(float theAngle, float theRadius, float TheColdDownTime)
+    {
+        angle = theAngle;
+        coldDownTime = TheColdDownTime;
+        radius = theRadius;
+    }
+    //Author: Allie Zhao
+    //The bullet will rotated around the player/monster
+    //angle----begining angle
+    //radius----the distance between shotter and bullet
+    public void GoAround()//Allie
+    {
+        float x = Mathf.Cos(Mathf.Deg2Rad * (angle)) * radius + position.x;//center of the circle
+        float z = Mathf.Sin(Mathf.Deg2Rad * (angle)) * radius + position.z;
+        //Vector3 temp = new Vector3(x, 10, z);
+        //float y = terr.SampleHeight(temp);
+        Vector3 newPos = new Vector3(x, 1, z);
+        gameObject.transform.position = newPos;
+        angle += 1f;
+        coldDownTime -= Time.deltaTime;
+        if (coldDownTime <= 0)
+        {
+            DestroyBut();
+        }
+        //go closter
+       
+      
+
+    }
+    //Author: Allie Zhao
+    //destroy the bullet
+    private void DestroyBut()
+    {
+        gameObject.SetActive(false);
+
+    }
+
+    private void CollDect(Vector3 targetPosition, float targectRaadius)
+    {
+        float dist = Vector3.Distance(targetPosition, transform.position);
+        if (dist < butR + targectRaadius)
+        {//destory the bullet
+         //DestroyBut();
+            DestroyBut();
+        }
+    }
+}
