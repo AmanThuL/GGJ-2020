51cf83c66f4a972de5f1a944ca13c9d6249fd98c
Yuan Luo (RIT Student) <yl5209@ad.rit.edu>
2020-02-02 14:47:44 -0500

    update

--- Repair-Game/Assets/Rachel's Assets/Scripts/SceneGenerator.cs
+++ Repair-Game/Assets/Rachel's Assets/Scripts/SceneGenerator.cs
@@ -16,12 +16,16 @@ public class SceneGenerator : MonoBehaviour
         sceneArray = new ArrayList();
 
         // Add scenes here
-        sceneArray.Add("Alfie Test Scene");
-        sceneArray.Add("Allie Test Scene");
-        sceneArray.Add("Main");
-        sceneArray.Add("Rudy Test Scene");
-        sceneArray.Add("Rachel Test Scene");
-        sceneArray.Add("SampleScene");
+        sceneArray.Add("BasicRoomMs");
+        sceneArray.Add("BasicRoomP");
+        sceneArray.Add("Room_1Plants");
+        sceneArray.Add("Room_2Plants");
+        sceneArray.Add("Room_3Plants");
+        sceneArray.Add("Room_4Plant");
+        sceneArray.Add("Room_5Mar");
+        sceneArray.Add("Room_6Mar");
+        sceneArray.Add("Room_7Mar");
+        sceneArray.Add("Room_8Mar");
     }
 
     // Update is called once per frame
