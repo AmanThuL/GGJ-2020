26cad5c137de625fe2a7b2def16773d9d0b1fa5b
Yuan Luo (RIT Student) <yl5209@ad.rit.edu>
2020-02-02 17:44:52 -0500

    Hole Manager
    rt

---
+++ Repair-Game/Assets/HoleManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleManager : MonoBehaviour
+{
+
+    public GameObject melon;
+    public GameObject mug;
+    public GameObject sofa;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(GameStats.IsThereMug == false)
+        {
+            mug.SetActive(false);
+        }
+
+        if(GameStats.IsThereSofa == false)
+        {
+            sofa.SetActive(false);
+        }
+
+        if(GameStats.IsThereWatermelon == false)
+        {
+            melon.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
